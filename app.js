04522bd0241ffb11281284b2ecd1e854610c6898
oktobat <62067363+oktobat@users.noreply.github.com>
2021-12-28 15:51:52 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+// Registering Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js');
+}
+
+// Requesting permission for Notifications after clicking on the button
+// const button = document.getElementById('notifications');
+// button.addEventListener('click', () => {
+//   Notification.requestPermission().then((result) => {
+//     if (result === 'granted') {
+//       randomNotification();
+//     }
+//   });
+// });
+
+// Setting up random Notification
+// function randomNotification() {
+//   const randomItem = Math.floor(Math.random() * games.length);
+//   const notifTitle = games[randomItem].name;
+//   const notifBody = `Created by ${games[randomItem].author}.`;
+//   const notifImg = `./data/img/${games[randomItem].slug}.jpg`;
+//   const options = {
+//     body: notifBody,
+//     icon: notifImg,
+//   };
+//   new Notification(notifTitle, options);
+//   setTimeout(randomNotification, 30000);
+// }
+
+// Progressive loading images
+// const imagesToLoad = document.querySelectorAll('img[data-src]');
+// const loadImages = (image) => {
+//   image.setAttribute('src', image.getAttribute('data-src'));
+//   image.onload = () => {
+//     image.removeAttribute('data-src');
+//   };
+// };
+// if ('IntersectionObserver' in window) {
+//   const observer = new IntersectionObserver((items) => {
+//     items.forEach((item) => {
+//       if (item.isIntersecting) {
+//         loadImages(item.target);
+//         observer.unobserve(item.target);
+//       }
+//     });
+//   });
+//   imagesToLoad.forEach((img) => {
+//     observer.observe(img);
+//   });
+// } else {
+//   imagesToLoad.forEach((img) => {
+//     loadImages(img);
+//   });
+// }
---
+++ sw.js
@@ -0,0 +1,44 @@
+// self.importScripts('./data/games.js');
+
+// Files to cache
+const cacheName = 'starbucks';
+const appShellFiles = [
+  './',
+  './index.html',
+  './css/index.css',
+  './favicon.ico',
+  './img/js13kgames.png',
+  './img/bg.png',
+  './icons/icon-32.png',
+  './icons/icon-64.png',
+  './icons/icon-96.png',
+  './icons/icon-128.png',
+  './icons/icon-168.png',
+  './icons/icon-192.png',
+  './icons/icon-256.png',
+  './icons/icon-512.png',
+];
+
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
